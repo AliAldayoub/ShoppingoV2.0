4fa11f9d5fd11f2b726f47a5042074cc304518f9
AliAldayoub <ali.dy111234@gmail.com>
2023-06-01 05:50:32 -0500

    add seller and product models

---
+++ models/seller.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const sellerSchema = new Schema({
+	user: {
+		type: Schema.Types.ObjectId,
+		ref: 'User',
+		required: true
+	},
+	storeImageURL: {
+		type: String
+	},
+	storeName: {
+		type: String,
+		required: true
+	},
+	storePhoneNumber: {
+		type: String,
+		required: true
+	},
+	location: {
+		type: String,
+		required: true
+	},
+	coo: {
+		type: Array,
+		required: true
+	},
+	paymentMethod: [
+		{
+			type: String,
+			enum: [ 'wepay', 'on delivery' ],
+			default: 'on delivery'
+		}
+	],
+	wepayCode: {
+		type: String,
+		required: function() {
+			return this.paymentMethod.includes('wepay');
+		}
+	}
+});
+
+const Seller = mongoose.model('Seller', sellerSchema);
+
+module.exports = Seller;
